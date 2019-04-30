03082cf840e7a8e19c0d11971abb28b008e1fd7b
AudreyJones <audrey.eliz.jones@gmail.com>
2019-04-30 13:40:19 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -9,8 +9,24 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-
+require 'pry'
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+  
+  def self.search(string)
+    if string.length == 0
+      Student.all
+    else
+      @students = []
+      Student.all.each do |student|
+        if student.name.include?(string.capitalize) == true
+          @students << student
+        else
+          #Do nothing!
+        end
+      end
+      @students
+    end
+  end
 end
