0eceb7915d673b231d1fe7e5124b3a01db2674d3
AudreyJones <audrey.eliz.jones@gmail.com>
2019-05-01 12:21:33 -0400

    Done.

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,6 +1,9 @@
+require 'pry'
 class ClassroomsController < ApplicationController
   def show
+    binding.pry
     @classroom = Classroom.find(params[:id])
+  
     @student = @classroom.oldest_student
   end
 
